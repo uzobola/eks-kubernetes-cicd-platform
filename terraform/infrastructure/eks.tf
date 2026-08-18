997469e29cad92c7a740fbac21689332241504d6
Uzo Bolarinwa <uzobolar@gmail.com>
2026-08-18 03:42:39 -0500

    Update eks.tf

--- terraform/infrastructure/eks.tf
+++ terraform/infrastructure/eks.tf
@@ -231,3 +231,15 @@ resource "aws_eks_node_group" "main" {
     Name = "${var.project_name}-nodes"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "eks_api_from_jenkins" {
+  security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+
+  referenced_security_group_id = aws_security_group.jenkins.id
+
+  description = "Allow Jenkins controller to reach EKS private API"
+
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+}
